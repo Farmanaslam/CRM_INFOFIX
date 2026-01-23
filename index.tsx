fee0c62a3ffde195c6308b35e99bc73d9983bd0a
Farman <farmanaslam674@gmail.com>
2026-01-23 12:06:05 +0530

    new

--- index.tsx
+++ index.tsx
@@ -1,26 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-// ✅ PWA: Register Service Worker
-{/*if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then((reg) => {})
-      .catch((err) => {
-        console.error("❌ Service Worker registration failed:", err);
-      });
+// ✅ UNREGISTER Service Worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => {
+      registration.unregister();
+      console.log("✅ Service Worker unregistered");
+    });
   });
-}*/}
+}
